7bf1ba43facab68903fddeff23fc845943a506ca
dykim <kdy36412@gmail.com>
2023-04-28 18:03:16 +0900

    2030428

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ require("dotenv").config();
 const mysql = require("mysql");
 const bodyParser = require("body-parser");
 const url = require("url");
+var session = require("express-session");
+app.use(
+  session({ secret: "mySecret", resave: false, saveUninitialized: false })
+);
 
 //application/urlencoded
 app.use(express.urlencoded({ extended: true }));
@@ -22,15 +26,44 @@ const connection = mysql.createConnection({
 
 app.get("/", (req, res) => res.send("eos8"));
 
-app.post("/register", (req, res) => {
-  // eos 회원가입시 정보 입력
+app.post("/myjob", (req, res) => {
+  var body = req.body;
+
+  var sql =
+    "SELECT HostName FROM aspmt m, hostmt h WHERE ASPNAME = ? AND siteid = ? AND m.HostID = h.HostID";
+
+  var params = [body.aspname, body.siteid, body.userid];
+  connection.query(sql, params, function (err, rows, fields) {
+    if (err) console.log(" 실패 \n" + err);
+    else body.hostname = rows[0].HostName;
+    console.log("1번" + rows[0].HostName);
+    console.log("2번" + req.body.hostname);
+    console.log(req.body);
+    //var hostname = rows[0].HostName;
+    req.params.message = rows[0].HostName;
+    req.session.hostname = rows[0].HostName;
+    //res.send({ hostname: rows[0].HostName });
+    //req.sesseion.hostname = "123";
+    res.redirect(307, "/login");
+    console.log(req.body);
+  });
+});
+
+app.post("/login", (req, res) => {
   var body = req.body;
   var sql2 = "SELECT UserReal FROM usermt WHERE UserID=? and SiteID = ?";
   var params = [body.userid, body.siteid];
-  //console.log(req.body.hostname);
+  const {
+    body: { userid, siteid, aspname, hostname },
+  } = req;
+  console.log("3번" + userid, siteid, aspname, hostname);
+  console.log(req.body);
+  var msg = req.session.message;
+  console.log("123" + msg);
+  console.log(req.session.hostname);
 
   const conn2 = mysql.createConnection({
-    host: req.body.hostname,
+    host: req.session.hostname,
     user: process.env.DBUN,
     password: process.env.DBPW,
     database: process.env.DATABASE,
@@ -45,23 +78,6 @@ app.post("/register", (req, res) => {
   });
 });
 
-app.post("/myjob", (req, res) => {
-  // eos 회원가입시 정보 입력
-  var body = req.body;
-  var sql =
-    "SELECT HostName FROM aspmt m, hostmt h WHERE ASPNAME = ? AND siteid = ? AND m.HostID = h.HostID";
-
-  var params = [body.aspname, body.siteid, body.userid];
-  connection.query(sql, params, function (err, rows, fields) {
-    if (err) console.log(" 실패 \n" + err);
-    else body.hostname = rows[0].HostName;
-    console.log("1번" + rows[0].HostName);
-    console.log("2번" + req.body.hostname);
-    res.redirect(307, "/register");
-  });
-});
-
 app.listen(port, () => {
   console.log(`server is listening at localhost:${process.env.PORT}`);
-  //console.log("server is listening at localhost:" + port);
 });
