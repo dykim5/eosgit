a0ccbcadf2f2566c239fcfdea1d48f3ee2153a92
dykim <kdy36412@gmail.com>
2023-05-12 15:57:29 +0900

    18ver

---
+++ client/src/_reducers/index.js
@@ -0,0 +1,10 @@
+import { createStore, combineReducers } from "redux";
+//import user from "./user_reducer";
+
+const rootReducer = combineReducers({
+  //user,
+});
+
+const store = createStore(rootReducer);
+
+export default rootReducer;
--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,31 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import "andtd/dist/antd.css";
+// import "antd/dist/antd.css";  //버전5부터 import 안해도 됨
 import { Provider } from "react-redux";
-
+import { applyMiddleware, createStore } from "redux";
+import promiseMiddleware from "redux-promise";
+import ReduxThunk from "redux-thunk";
+import Recuder from "./_reducers";
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+root.render(<App />);
+
+const createStoreWithMiddleware = applyMiddleware(
+  promiseMiddleware,
+  ReduxThunk
+)(createStore);
+
+ReactDOM.render(
+  <Provider
+    store={createStoreWithMiddleware(
+      Recuder,
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )}
+  ></Provider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
